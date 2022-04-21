804f7787b99df4c50441d6902d5337841c53f8b4
natsukiromero <103156146+natsukiromero@users.noreply.github.com>
2022-04-21 09:06:43 -0700

    testing

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -6,4 +6,12 @@ public class MarkdownParseTest {
     public void addition() {
         assertEquals(2, 1 + 1);
     }
+
+    @Test
+    /**calls getLinks on test-file.md
+     * assertequals [https://something.com, some-page.html] 
+    */
+    public void getLinksTester() throws IOException{
+        //assertEquals(MarkdownParse.main(testFile).links, expected);
+    }
 }
